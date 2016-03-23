3d4ce5d384f495eb99658bcdbdfc2fcec0e288f5
yeshida <15980598149@139.com>
2016-03-23 17:57:01 +0800

    first

---
+++ app/src/main/java/com/example/administrator/myapplication/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/3/23.
+ */
+public class LoginActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.index);
+    }
+}
